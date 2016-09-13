e515fc6816876a8efdc28a303e0dbced77366989
nacho <nacho@limenius.com>
2016-09-13 19:06:43 +0200

    Build scripts

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+'use strict'
+var webpack = require('webpack')
+
+var env = process.env.NODE_ENV
+
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+}
+
+var reduxExternal = {
+  root: 'Redux',
+  commonjs2: 'redux',
+  commonjs: 'redux',
+  amd: 'redux'
+}
+
+var reactReduxExternal = {
+  root: 'ReactRedux',
+  commonjs2: 'react-redux',
+  commonjs: 'react-redux',
+  amd: 'react-redux'
+}
+
+var config = {
+  externals: {
+    'react': reactExternal,
+    'redux': reduxExternal,
+    'react-redux': reactReduxExternal
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'ReduxForm',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+  config.plugins.push(new webpack.optimize.DedupePlugin())
+}
+
+module.exports = config
