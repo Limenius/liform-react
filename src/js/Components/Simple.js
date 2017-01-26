dce5e4548133b6f5c890c2a7ea915f1baf420ea6
Nacho Martín <nacho@limenius.com>
2017-01-26 19:40:06 +0100

    Improve first example

--- src/js/Components/Simple.js
+++ src/js/Components/Simple.js
@@ -8,14 +8,47 @@ const Demo = () => {
     const reducer = combineReducers({ form: formReducer })
     const store = createStore(reducer)
     const schema = {
+        'type':'object',
+        'properties':{
+            'title':{
+                'type':'string',
+                'title':'Some text',
+                'description':'This is a help message',
+            },
+            'description':{
+                'type':'string',
+                'format':'textarea',
+                'title':'Description',
+                'default':'A description...',
+            }
+        }
+    }
+    return (
+        <Provider store={store}>
+            <Liform schema={schema} onSubmit={(v) => {console.log(v)}}/>
+        </Provider>
+    )
+}
+
+const Code = () => (
+    <pre>{`
+    import React from 'react'
+    import { createStore, combineReducers } from 'redux'
+    import { reducer as formReducer } from 'redux-form'
+    import { Provider } from 'react-redux'
+    import Liform from 'liform-react'
+
+    const reducer = combineReducers({ form: formReducer })
+    const store = createStore(reducer)
+    const schema = {
         "type":"object",
         "properties":{
-            "someText":{
+            "title":{
                 "type":"string",
                 "title":"Some text",
                 "description":"This is a help message",
             },
-            "someTextarea":{
+            "description":{
                 "type":"string",
                 "format":"textarea",
                 "title":"Description",
@@ -25,26 +58,26 @@ const Demo = () => {
     }
     return (
         <Provider store={store}>
-            <Liform schema={schema} />
+            <Liform schema={schema} onSubmit={(v) => {console.log(v)}}/>
         </Provider>
     )
-}
+    `}</pre>
+)
 
 const Json = () => (
     <pre>{`
     {
         "type":"object",
         "properties":{
-            "someText":{
+            "title":{
                 "type":"string",
                 "title":"Some text",
                 "description":"This is a help message",
                 "propertyOrder":2
             },
-            "someTextarea":{
+            "description":{
                 "type":"string",
                 "format":"textarea",
-                "title":"Description",
                 "default":"A description...",
                 "propertyOrder":3
             }
@@ -54,10 +87,10 @@ const Json = () => (
 
 const Simple = () => (
   <div>
-    <h2>Simple</h2>
+    <h2>Simple Example</h2>
     <div className="row">
         <div className="col-md-6">
-            <Json/>
+            <Code/>
         </div>
         <div className="col-md-6">
             <Demo/>
