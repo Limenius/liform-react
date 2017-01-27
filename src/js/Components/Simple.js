2e1692e245bd1b6f701812b15719817a7b64cd80
Nacho Martín <nacho@limenius.com>
2017-01-27 12:43:02 +0100

    Better first example and general improvements

--- src/js/Components/Simple.js
+++ src/js/Components/Simple.js
@@ -3,6 +3,7 @@ import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { Provider } from 'react-redux'
 import Liform from 'liform-react'
+const code = require('./Home.md');
 
 const Demo = () => {
     const reducer = combineReducers({ form: formReducer })
@@ -31,71 +32,18 @@ const Demo = () => {
 }
 
 const Code = () => (
-    <pre>{`
-    import React from 'react'
-    import { createStore, combineReducers } from 'redux'
-    import { reducer as formReducer } from 'redux-form'
-    import { Provider } from 'react-redux'
-    import Liform from 'liform-react'
-
-    const reducer = combineReducers({ form: formReducer })
-    const store = createStore(reducer)
-    const schema = {
-        "type":"object",
-        "properties":{
-            "title":{
-                "type":"string",
-                "title":"Some text",
-                "description":"This is a help message",
-            },
-            "description":{
-                "type":"string",
-                "format":"textarea",
-                "title":"Description",
-                "default":"A description...",
-            }
-        }
-    }
-    return (
-        <Provider store={store}>
-            <Liform schema={schema} onSubmit={(v) => {console.log(v)}}/>
-        </Provider>
-    )
-    `}</pre>
-)
-
-const Json = () => (
-    <pre>{`
-    {
-        "type":"object",
-        "properties":{
-            "title":{
-                "type":"string",
-                "title":"Some text",
-                "description":"This is a help message",
-                "propertyOrder":2
-            },
-            "description":{
-                "type":"string",
-                "format":"textarea",
-                "default":"A description...",
-                "propertyOrder":3
-            }
-        }
-    }`}</pre>
+    <div>
+         <div dangerouslySetInnerHTML={{ __html: code }}/>
+    </div>
 )
 
 const Simple = () => (
   <div>
-    <h2>Simple Example</h2>
-    <div className="row">
-        <div className="col-md-6">
-            <Code/>
-        </div>
-        <div className="col-md-6">
-            <Demo/>
-        </div>
-    </div>
+    <h2>Simple Form</h2>
+    <h3>Form</h3>
+    <Demo/>
+    <h3>Code</h3>
+    <Code/>
   </div>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ let config = {
             { test: /\.scss$/i, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ] },
             { test: /\.css$/i, loaders: [ 'style-loader', 'css-loader','sass-loader' ] },
             { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.md$/, loader: 'html!highlight!markdown' },
             { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
