6c72df4a16469e553f761ec040be0aca5cf80373
Nacho Martín <nacho@limenius.com>
2017-02-02 17:38:54 -0500

    Better ordering

--- src/renderFields.js
+++ src/renderFields.js
@@ -13,11 +13,11 @@ const renderFields =
     {
         let propertyNames = Object.keys(schema.properties)
         propertyNames.sort((a, b) => {
-            if (!a.properyOrder) {
-                return 1
-            }
             if (!b.properyOrder) {
                 return -1
+            }
+            if (!a.properyOrder) {
+                return 1
             }
             return a.propertyOrder - b.propertyOrder
         })
