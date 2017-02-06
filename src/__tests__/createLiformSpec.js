ad1f786a19dae1f0effc5648b192d02a6bb77847
Nacho Martín <nacho@limenius.com>
2017-02-06 17:25:34 -0500

    Improve tests

--- src/__tests__/createLiformSpec.js
+++ src/__tests__/createLiformSpec.js
@@ -1,8 +1,7 @@
-import expect, {createSpy} from 'expect';
-import React, {Component, PropTypes} from 'react';
-import TestUtils from 'react-addons-test-utils';
-import Liform from '../liform';
-import DefaultTheme from '../themes/bootstrap3';
+import expect from 'expect'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import Liform from '../'
 
 
 describe('createLiform', () => {
@@ -13,24 +12,24 @@ describe('createLiform', () => {
                 type: 'string',
             }
         }
-    };
+    }
 
-    const schemaWrong = {
-        title: 'A schema',
-        properties: {
-            'name' : {
-                type: 'asdf',
-            }
-        }
-    };
+    //const schemaWrong = {
+    //    title: 'A schema',
+    //    properties: {
+    //        'name' : {
+    //            type: 'asdf',
+    //        }
+    //    }
+    //}
 
     it('should render a form', () => {
-        const shallowRenderer = TestUtils.createRenderer();
-        var li = React.createElement(Liform, {schema: schema});
-        shallowRenderer.render(React.createElement(Liform, {schema: schema}));
+        const shallowRenderer = TestUtils.createRenderer()
+        //let li = React.createElement(Liform, { schema: schema })
+        shallowRenderer.render(React.createElement(Liform, { schema: schema }))
 
-        const component = shallowRenderer.getRenderOutput();
-        expect(component.type).toBeA('function');
-    });
+        const component = shallowRenderer.getRenderOutput()
+        expect(component.type).toBeA('function')
+    })
 
-});
+})
