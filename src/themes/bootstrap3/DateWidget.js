bc49c8fbe43b851ceca2b07cdec48726f703e996
Nacho Martín <nacho@limenius.com>
2017-02-06 12:02:59 -0500

    Widget for times

---
+++ src/themes/bootstrap3/DateWidget.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BaseInputWidget from './BaseInputWidget'
+
+const DateWidget = (props) => {
+    return (
+        <BaseInputWidget type="date" {...props} />
+    )
+}
+
+export default DateWidget
