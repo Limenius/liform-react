74d215285f7a11395b89ba3d6f3dfbdc0fd299e0
Nacho Martín <nacho@limenius.com>
2017-02-10 17:52:32 +0100

    Improved process submit errors

--- src/processSubmitErrors.js
+++ src/processSubmitErrors.js
@@ -1,14 +1,14 @@
-import _ from 'lodash'
 import { SubmissionError } from 'redux-form'
 
 const processSubmitErrors = response => {
     let errors = {}
     if (response.hasOwnProperty('errors')) {
-        _.forIn(response.errors.children, (value, field) => {
+        for (let field in response.errors.children) {
+            let value = response.errors.children[field]
             if (value.hasOwnProperty('errors'))  {
                 errors[field] = value.errors[0]
             }
-        })
+        }
         throw new SubmissionError(errors)
 
     }
