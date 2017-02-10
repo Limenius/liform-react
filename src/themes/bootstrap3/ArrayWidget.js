cc19c07449746b95bc3fee6db1e90115cd5b1327
Nacho Martín <nacho@limenius.com>
2017-02-11 00:28:35 +0100

    Fix array widget for initial values

--- src/themes/bootstrap3/ArrayWidget.js
+++ src/themes/bootstrap3/ArrayWidget.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderField from '../../renderField'
-import { FieldArray } from 'redux-form'
+import { FieldArray, formValueSelector } from 'redux-form'
+import { connect } from 'react-redux'
 import _ from 'lodash'
 import ChoiceWidget from './ChoiceWidget'
 
@@ -24,10 +25,12 @@ const renderArrayFields = (count, schema, theme, fieldName, remove) => {
 }
 
 const renderInput = field => {
+    console.log('in input')
+    console.log(field)
     return (
         <div className="arrayType form-group">
             <legend className="control-label" >{field.label}</legend>
-            { renderArrayFields(field.fields.length, field.schema.items, field.theme, field.name, (idx) => field.fields.remove(idx)) }
+            { renderArrayFields(field.values.length, field.schema.items, field.theme, field.fieldName, (idx) => field.fields.remove(idx)) }
             <button type="button" className="pull-right btn btn-primary" onClick={() => field.fields.push({})}>Add</button>
             <div className="clearfix"/>
         </div>
@@ -39,19 +42,30 @@ const CollectionWidget = props =>  {
         <FieldArray
             component={renderInput}
             label={props.label}
-            name={props.fieldName}
+            fieldName={props.fieldName}
             schema={props.schema}
+            values={props.values}
             theme={props.theme}
         />
     )
 }
 
+
 const ArrayWidget = props =>  {
+    console.log(props)
     // Arrays are tricky because they can be multiselects or collections
     if (props.schema.items.hasOwnProperty('enum') && props.schema.hasOwnProperty('uniqueItems') && props.schema.uniqueItems) {
         return ChoiceWidget({ ...props, schema: props.schema.items, multiple: true })
     } else {
-        return CollectionWidget(props)
+        const selector = formValueSelector('form')
+        const ConnectedCollectionWidget = connect(
+            state => {
+                return {
+                    values: selector(state, props.fieldName),
+                }
+            }
+        )(CollectionWidget);
+        return <ConnectedCollectionWidget {...props}/>
     }
 }
 
