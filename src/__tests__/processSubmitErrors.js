4322606eb970febd6bcfc084e5f948f69d7eecc9
Nacho Martín <nacho@limenius.com>
2017-03-04 19:09:43 +0100

    Test processSubmitErrors

--- src/__tests__/processSubmitErrors.js
+++ src/__tests__/processSubmitErrors.js
@@ -3,25 +3,35 @@ import processSubmitErrors from '../processSubmitErrors'
 import { SubmissionError } from 'redux-form'
 
 describe('processSubmitErrors', () => {
-    const response = {
-        'code' : null,
-        'message' : 'Validation Failed',
-        'errors':
-        {
-            'children':
-            {
-                'name': {
-                    'errors': [ 'This value should not be equal to \'Mary\'.' ]
-                },
-                'color' : []
-            }
-        }
-    }
+
 
     it('raises exception if there is an error', () => {
+        const response = {
+            'code' : null,
+            'message' : 'Validation Failed',
+            'errors':
+            {
+                'children':
+                {
+                    'name': {
+                        'errors': [ 'This value should not be equal to \'Mary\'.' ]
+                    },
+                    'color' : []
+                }
+            }
+        }
         expect(function () {
             processSubmitErrors(response)
         }).toThrow(SubmissionError)
     })
 
+    it('does not raise exception if there is no error', () => {
+        const response = {
+            'code' : null,
+            'message' : 'Validation Failed'
+        }
+        expect(function () {
+            processSubmitErrors(response)
+        }).toNotThrow(SubmissionError)
+    })
 })
