afdeb5bcdb4789c9758fa6bc2698ace67f6cf06b
gaston <gastonnkh@gmail.com>
2017-08-16 14:28:09 +0100

    [ajv] added support for custom instance of ajv from users

--- src/buildSyncValidation.js
+++ src/buildSyncValidation.js
@@ -3,8 +3,30 @@ import Ajv from 'ajv'
 
 const setError = (errors, error) => {
     const dataPathParts = error.dataPath.split('.').slice(1)
+    //  regular expression to  get the object path and index from the ajv error data path
+    const re = new RegExp(/(^\w+)\[([0-9]+)\]/, 'gm')
 
     dataPathParts.reduce((errors, part, index) => {
+        let res = re.exec(part)
+        
+        if(res && res.length>0){
+            let p = res[1]
+            let i = res[2]
+            if(!errors[p]){
+                errors[p] = []
+            }
+            if(typeof errors[p]=='string'){
+                let err = errors[p]
+                errors[p] = []
+                errors[p]._error = err
+            }
+            if (index === dataPathParts.length -1) {
+                errors[p][i] = error.message
+            }else{
+                errors[p][i] = errors[p][i] || {}
+            }
+            return errors[p][i]
+        }
         if (index === dataPathParts.length -1) {
             errors[part] = error.message
         }
@@ -13,12 +35,13 @@ const setError = (errors, error) => {
         }
         return errors[part]
     }, errors)
+    console.log(errors)
     return errors
 }
 
 
-const buildSyncValidation = schema => {
-    const ajv = new Ajv({ errorDataPath: 'property', allErrors: true })
+const buildSyncValidation = (schema, 
+    ajv = new Ajv({ errorDataPath: 'property', allErrors: true }) ) => {
     return values => {
         let errors = {}
         const valid = ajv.validate(schema, values)
