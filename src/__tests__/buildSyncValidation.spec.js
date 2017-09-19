7214f779cd7433bac9f6935cd58958de5f20935f
Nacho Martín <nacho@limenius.com>
2017-09-19 18:07:30 +0200

    Refactored sync validation. [Fixes #25]

--- src/__tests__/buildSyncValidation.spec.js
+++ src/__tests__/buildSyncValidation.spec.js
@@ -1,11 +1,92 @@
 import expect from 'expect'
-import { setError } from '../buildSyncValidation.js'
+import buildSyncValidation, { setError } from '../buildSyncValidation.js'
+import Ajv from 'ajv'
 
-describe('setError', () => {
+describe('sync validation', () => {
 
     it('Converts between ajv format and object', () => {
         const errors = setError({}, { dataPath: '.one.two', message: 'I\'m an error' })
         expect(errors).toBeA('object').toIncludeKey('one')
 
     })
+    it('Works with basic objects', () => {
+        let schema = {
+            properties: {
+                name : {
+                    type: 'string',
+                    minLength: 3,
+                },
+            },
+            required:["name"]
+        }
+
+        let values = {}
+        let errors = buildSyncValidation(schema)(values)
+        expect(errors).toBeA('object').toIncludeKey('name')
+    })
+    it('Errors on arrays are in _error key of the array', () => {
+        let schema = {
+            properties: {
+                columns: {
+                    type: "array",
+                    minItems: 1,
+                    items: {
+                        type: "string"
+                    }
+                },
+            },
+            required:["columns"]
+        }
+
+        let values = {}
+        let errors = buildSyncValidation(schema)(values)
+        expect(errors).toBeA('object').toIncludeKey('columns')
+        expect(errors.columns).toBeA('object').toIncludeKey('_error')
+    })
+    it('Works with array elements', () => {
+        let schema = {
+            properties: {
+                columns: {
+                    type: "array",
+                    minItems: 1,
+                    items: {
+                        type: "string",
+                        minLength: 3
+                    }
+                },
+            },
+            required:["columns"]
+        }
+
+        let values = {columns: ['a']}
+        let errors = buildSyncValidation(schema)(values)
+        expect(errors).toBeA('object').toIncludeKey('columns')
+        expect(errors.columns).toBeA('object').toIncludeKey('0')
+    })
+    it('Works with several errors', () => {
+        let schema = {
+            properties: {
+                name: {
+                    type: "string",
+                    minLength: 3,
+                },
+                columns: {
+                    type: "array",
+                    minItems: 1,
+                    items: {
+                        type: "string",
+                        minLength: 3
+                    }
+                },
+            },
+            required:["columns", "name"]
+        }
+
+        let values = {columns: ['a'], name: 'aa'}
+        let errors = buildSyncValidation(schema)(values)
+        expect(errors).toBeA('object').toIncludeKey('columns')
+        expect(errors).toBeA('object').toIncludeKey('name')
+        expect(errors.columns).toBeA('object').toIncludeKey('0')
+    })
 })
+
