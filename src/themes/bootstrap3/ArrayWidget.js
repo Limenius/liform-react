083edc6bc3d4b24a18bb6459eddf40f8e0a787c5
Nacho Martín <nacho@limenius.com>
2017-09-19 16:12:35 +0200

    Fix ArrayWidget display error of array

--- src/themes/bootstrap3/ArrayWidget.js
+++ src/themes/bootstrap3/ArrayWidget.js
@@ -44,15 +44,15 @@ const renderArrayFields = (count, schema, theme, fieldName, remove, context, swa
 const renderInput = field => {
     const className = classNames([
         'arrayType',
-        { 'has-error' : field.meta.touched && field.meta.error }
+        { 'has-error' : field.meta.submitFailed && field.meta.error }
     ])
 
     return (
         <div className={className}>
             <legend className="control-label" >{field.label}</legend>
-            {field.meta.touched && field.meta.error && <span className="help-block">{field.meta.error}</span>}
+            {field.meta.submitFailed && field.meta.error && <span className="help-block">{field.meta.error}</span>}
             { renderArrayFields(field.fields.length, field.schema.items, field.theme, field.fieldName, (idx) => field.fields.remove(idx), field.context, (a, b) => {field.fields.swap(a,b)}) }
-            <button type="button" className="pull-right btn btn-primary" onClick={() => field.fields.push(null)}>Add</button>
+            <button type="button" className="pull-right btn btn-primary" onClick={() => field.fields.push()}>Add</button>
             <div className="clearfix"/>
         </div>
     )
